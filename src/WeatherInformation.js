efe4158214846502dbe0bd2e0dae2b9ef45bb6c5
MariaLizMatthaeus <94976037+MariaLizMatthaeus@users.noreply.github.com>
2022-06-11 17:21:54 +0200

    added css changes

--- src/WeatherInformation.js
+++ src/WeatherInformation.js
@@ -5,7 +5,7 @@ import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInformation(props) {
   return (
-    <div className="WeatherInfo">
+    <div className="WeatherInformation">
       <h1>{props.data.city}</h1>
       <ul>
         <li>
@@ -14,18 +14,19 @@ export default function WeatherInformation(props) {
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6">
+        <div className="col-6 col-md-4">
           <div className="clearfix">
             <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={52} />
-            </div>
-
-            <div className="float-left">
-              <WeatherTemperature celsius={props.data.temperature} />
+              <span>
+                <WeatherIcon code={props.data.icon} size={52} />
+              </span>
+              <span>
+                <WeatherTemperature celsius={props.data.temperature} />
+              </span>
             </div>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-6 col-md-4">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
